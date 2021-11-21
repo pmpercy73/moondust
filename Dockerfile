990ac9dc19b51028d07d7946a9d91b8c03745b43
pmpercy73 <94786275+pmpercy73@users.noreply.github.com>
2021-11-21 09:02:56 -0500

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,7 @@ RUN set -ex \
         && apk update \
         && apk upgrade \
         && apk add --no-cache --virtual .build-deps ca-certificates git nginx curl wget unzip \
-        && mkdir /tmp/v2ray \
-        && git clone https://github.com/xiongbao/we.dog \
-        && mv we.dog/* /var/lib/nginx/html/ \
-        && rm -rf /we.dog
+
 ADD entrypoint.sh /entrypoint.sh
 
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ ! -e '/usr/bin/v2ray' ]; then
-    curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+    mkdir /tmp/v2ray
+	curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
     unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
     chmod +x /tmp/v2ray/v2ray
     mv /tmp/v2ray/v2ray /usr/bin/v2ray
@@ -14,7 +15,8 @@ cat << EOF > /root/config.json
   },
   "inbounds": [
   {
-    "port": $Nginx_PORT,
+    "port": $RAY_PORT,
+    "listen":"127.0.0.1",
     "protocol": "vmess",
     "settings": {
       "clients": [
@@ -42,26 +44,26 @@ cat << EOF > /root/config.json
 EOF
 
 # generate Nginx default config file
-# cat << EOF > /etc/nginx/conf.d/default.conf
-# server {
-    # listen $Nginx_PORT default_server;
-    # charset utf-8;
+cat << EOF > /etc/nginx/conf.d/default.conf
+server {
+    listen $Nginx_PORT default_server;
+    charset utf-8;
 
-    # location /$RAY_PATH {
-    # proxy_redirect off;
-    # proxy_pass http://127.0.0.1:$RAY_PORT;
-    # proxy_http_version 1.1;
-    # proxy_set_header Upgrade \$http_upgrade;
-    # proxy_set_header Connection "upgrade";
-    # proxy_set_header Host \$http_host;
-    # }
+    location /$RAY_PATH {
+    proxy_redirect off;
+    proxy_pass http://127.0.0.1:$RAY_PORT;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection "upgrade";
+    proxy_set_header Host \$http_host;
+    }
 
-# }
-# EOF
+}
+EOF
 
 
 # Start nginx
-#nginx
+nginx
 
 # Run V2Ray
 v2ray -config /root/config.json
